435ad5bb43898d2ae3efbec473cd48b0102da11c
MatthewMcDade13 <Matthew.McDade13@gmail.com>
2018-05-14 01:35:50 -0700

    Contracted AIDS and finally got rect to move with live input

--- src/GameLoop.hs
+++ src/GameLoop.hs
@@ -8,12 +8,36 @@ import SDL.Vect
 import qualified SDL
 import Foreign.C.Types
 import System.IO
+import Control.Monad.State
+import Control.Monad.IO.Class
 
 data Context = Context SDL.Window SDL.Renderer
 
 move :: SDL.Rectangle CInt -> V2 CInt -> SDL.Rectangle CInt
-move (SDL.Rectangle (SDL.P (V2 rx ry)) (V2 xx yy)) (V2 ox oy) = 
-    SDL.Rectangle (SDL.P (V2 (rx + ox) (ry + oy))) (V2 xx yy)
+move (SDL.Rectangle (SDL.P pos) size) offset = 
+    SDL.Rectangle (SDL.P (pos + offset)) (size)
+
+
+
+move' :: V2 CInt -> StateT (SDL.Rectangle CInt) IO ()
+move' offset = state $ \r -> ((), move r offset)
+
+defaultRect :: SDL.Rectangle CInt
+defaultRect = SDL.Rectangle (SDL.P (V2 0 0)) (V2 0 0)
+
+proccessEvents' :: StateT (SDL.Rectangle CInt) IO ()
+proccessEvents' = do
+    left <- liftIO (isKeyPressed' SDL.ScancodeLeft)
+    right <- liftIO (isKeyPressed' SDL.ScancodeRight)
+    up <- liftIO (isKeyPressed' SDL.ScancodeUp)
+    down <- liftIO (isKeyPressed' SDL.ScancodeDown)
+    if left then move' (V2 (-1) 0) else noop
+    if right then move' (V2 1 0) else noop
+    if up then move' (V2 0 (-1)) else noop
+    if down then move' (V2 0 1) else noop
+
+    where noop = move' (V2 0 0)
+    
 
 
 gameLoop :: SDL.Rectangle CInt -> SDL.Renderer -> IO ()
@@ -21,31 +45,44 @@ gameLoop rect renderer = do
     timer <- Timer.createTimer
     events <- SDL.pollEvents
 
-    let checkKey k = any (isKeyPressed k) events 
-        isEscPressed = checkKey SDL.KeycodeEscape
-        isLeftPressed = checkKey SDL.KeycodeLeft
-        isRightPressed = checkKey SDL.KeycodeRight
-        isUpPressed = checkKey SDL.KeycodeUp
-        isDownPressed = checkKey SDL.KeycodeDown
-        newRect = if isLeftPressed
-                  then move rect (V2 (-5) 0)
-                  else if isRightPressed
-                  then move rect (V2 5 0)
-                  else if isUpPressed 
-                  then move rect (V2 0 (-5))
-                  else if isDownPressed
-                  then move rect (V2 0 5)
-                  else rect
+    (_, newRect) <- runStateT proccessEvents' rect
 
+    let isEscPressed = any (isKeyPressed SDL.KeycodeEscape) events
 
-    SDL.rendererDrawColor renderer SDL.$= V4 255 0 0 0   
-    print newRect            
+    print newRect
+                  
+    SDL.clear renderer
+    SDL.rendererDrawColor renderer SDL.$= V4 255 0 0 0       
     SDL.fillRect renderer (Just newRect)
     SDL.rendererDrawColor renderer SDL.$= V4 0 0 0 0
-    SDL.clear renderer
     SDL.present renderer
 
     unless isEscPressed (gameLoop newRect renderer)
+
+    where 
+        proccessEvents acc e = 
+            case getKeyPressed e of
+                Just SDL.KeycodeLeft -> move acc (V2 (-5) 0)
+                Just SDL.KeycodeRight -> move acc (V2 5 0)
+                Just SDL.KeycodeUp -> move acc (V2 0 (-5))
+                Just SDL.KeycodeDown -> move acc (V2 0 5)
+                _ -> acc
+
+
+
+getKeyPressed :: SDL.Event -> Maybe SDL.Keycode
+getKeyPressed SDL.Event { SDL.eventPayload = ep } =
+    case ep of 
+        SDL.KeyboardEvent kbe ->
+            if SDL.keyboardEventKeyMotion kbe == SDL.Pressed
+            then Just $ SDL.keysymKeycode $ SDL.keyboardEventKeysym kbe
+            else Nothing
+        _ -> Nothing
+
+isKeyPressed' :: SDL.Scancode -> IO Bool
+isKeyPressed' sc = do
+    SDL.pumpEvents
+    SDL.getKeyboardState >>= (\f -> return (f sc))
 
 isKeyPressed :: SDL.Keycode -> SDL.Event -> Bool
 isKeyPressed key event =
